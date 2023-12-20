be0fac1ad29001997075b8d93619541206d48642
Tok68030 <99962418+Tok68030@users.noreply.github.com>
2023-12-20 19:09:08 +0900

    Create ovice-utils.js

---
+++ ovice-utils.js
@@ -0,0 +1,30 @@
+<!-- URLパラメータ引き継ぎ設定スクリプト　jQuery必須　-->
+
+var prm;
+var btn_position;
+
+function retrieveGETqs() {
+
+	var query = window.location.search.substring(1);
+	return query;
+	 /* 引数がない時は処理しない */
+	if (!query) return false;
+
+}
+
+// inperit query parameter
+$(function(){
+	jQuery('a').click(function() {
+	var target_url = $(this).attr("href");
+	var str = retrieveGETqs();
+	prm = decodeURIComponent(str);
+
+	if (prm) {
+     if (target_url.indexOf('?') != -1) {
+         $('a').attr('href', target_url + '&' + prm + '&lp_type=' + btn_position);
+       } else {
+         $('a').attr('href', target_url + '?' + prm + '&lp_type=' + btn_position);
+       }
+  }	
+ })	
+})
