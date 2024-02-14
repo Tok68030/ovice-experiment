f8e55e6325e19d750e2c97531fa78f83f1bcb0b3
Tok68030 <99962418+Tok68030@users.noreply.github.com>
2024-02-14 18:09:24 +0900

    Create ex.js build 001

---
+++ ex.js
@@ -0,0 +1,18 @@
+(function(){
+  if ((typeof sessionStorage !=== 'undefined') & (typeof localStorage !== 'undefined')) {
+    var ls = localStorage;
+    var ss = sessionStorage;
+    ls.setItem('ovicecom_cPages', Number(ls.getItem('ovicecom_cPages')) + 1);
+    if(ss.getItem('ovicecom_fEntry') === 'null') {
+      ss.setItem('ovicecom_fEntry', 1);
+      var v = Number(ls.getItem('ovicecom_cVisits'));
+      ss.setItem('ovicecom_cVisits', v + 1);
+      var r = document.referrer;
+      if (v === 1) {
+        ss.setItem('ovicecom_sFirstRef', r);
+      } else {
+        ss.setItem('ovicecom_sLastRef', r);
+      }
+    }
+  }
+})();
