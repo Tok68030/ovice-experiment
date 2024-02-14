19dbe078c052ae801212759eb71a30e2e4736cdc
Tok68030 <99962418+Tok68030@users.noreply.github.com>
2024-02-14 19:16:40 +0900

    Update ex.js

--- ex.js
+++ ex.js
@@ -3,10 +3,10 @@
     var ls = localStorage;
     var ss = sessionStorage;
     ls.setItem('ovicecom_cPages', Number(ls.getItem('ovicecom_cPages')) + 1);
-    if(ss.getItem('ovicecom_fEntry') === 'null') {
+    if(ss.getItem('ovicecom_fEntry') === null) {
       ss.setItem('ovicecom_fEntry', 1);
       var v = Number(ls.getItem('ovicecom_cVisits'));
-      ss.setItem('ovicecom_cVisits', v + 1);
+      ls.setItem('ovicecom_cVisits', v + 1);
       var r = document.referrer;
       if (v === 1) {
         ls.setItem('ovicecom_sFirstRef', r);
