73bdb74a3640f354baa97e784519ffbf2b671bb6
Tok68030 <99962418+Tok68030@users.noreply.github.com>
2024-02-16 17:33:17 +0900

    Update ex.js

--- ex.js
+++ ex.js
@@ -6,9 +6,10 @@ function retrieveGETqs() {
 
 function attr(d) {
   const refdata = `[
-    {"domain": "ovice.com", "ref": "ovice"},
-    {"domain": "ovice.in", "ref": "ovice"},
+    {"domain": "ovice.", "ref": "ovice"},
     {"domain": "flexergylab.com", "ref": "ovice"},
+    {"domain": "play.google.com", "ref": "aso_google"},
+    {"domain": "apps.apple.com", "ref": "aso_apple"},
     {"domain": "google.", "ref": "seo_google"},
     {"domain": "yahoo.co.jp", "ref": "seo_yahoo"}, 
     {"domain": "yahoo.com", "ref": "seo_yahoo"}, 
@@ -29,16 +30,40 @@ function attr(d) {
     {"domain": "infoseek.co.jp", "ref": "seo_infoseek"},
     {"domain": "auone.jp", "ref": "seo_au"},
     {"domain": "docomo.ne.jp", "ref": "seo_docomo"},
+    {"domain": "facebook.com", "ref": "social_facebook"},
+    {"domain": "x.com", "ref": "social_x"},
+    {"domain": "linkedin.com", "ref": "social_linkedin"},
+    {"domain": "youtube.com", "ref": "social_youtube"},
+    {"domain": "instagram.com", "ref": "social_instagram"},
     {"domain": "www.itreview.jp", "ref": "review_itreview"},
     {"domain": "boxil.jp", "ref": "review_boxil"},
     {"domain": "it-trend.jp", "ref": "review_it-trend"},
-    {"domain": "capterra.jp", "ref": "review_capterra"}
+    {"domain": "capterra.jp", "ref": "review_capterra"},
+    {"domain": "capterra.com", "ref": "review_capterra"},
+    {"domain": "g2.com", "ref": "review_g2"},
+    {"domain": "getapp.com", "ref": "review_getapp"},
+    {"domain": "prtimes.jp", "ref": "media_prtimes"},
+    {"domain": "wantedly.com", "ref": "media_wantedly"},
+    {"domain": "techable.jp", "ref": "media_techable"},
+    {"domain": "zendesk.com", "ref": "media_zendesk"},
+    {"domain": "patentsalon.com", "ref": "media_patentsalon"},
+    {"domain": "note.com", "ref": "media_note"},
+    {"domain": "connpas.com", "ref": "media_connpas"},
+    {"domain": "voice-ping.com", "ref": "media_voice-ping"},
+    {"domain": "toremaga.com", "ref": "media_toremaga"},
+    {"domain": "wmr.tokyo", "ref": "media_wmr-tokyo"},
+    {"domain": "zdnet.com", "ref": "media_zdnet"},
+    {"domain": "cnet.com", "ref": "media_cnet"},
+    {"domain": "impress.co.jp", "ref": "media_impress"},
+    {"domain": "panora.tokyo", "ref": "media_panora-tokyo"},
+    {"domain": "notion.site", "ref": "media_notion"}
   ]`;
   var j = JSON.parse(refdata);
   for (var i = 0; i < j.length; i++) {
     if (d.indexOf(j[i].domain) > -1) {return j[i].ref;}
   }
-  return "direct";
+  const u = new URL(d);
+  return 'other_' + u.host;
 }
 
 
@@ -64,5 +89,11 @@ function attr(d) {
     ls.setItem('ovicecom_sLastRef', r);
   }
   if (global_prm !== '') {
+    if (r === '') {
+      ls.setItem('ovicecom_attribution', 'direct');
+    } else {
+      console.log("r = " + r);
+      ls.setItem('ovicecom_attribution', attr(r));
+    }
   }
 })();
