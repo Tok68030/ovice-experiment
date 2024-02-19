17665b4a6237cd91eebe75a40c460b1534dee24d
Tok68030 <99962418+Tok68030@users.noreply.github.com>
2024-02-19 18:39:50 +0900

    Update ovice-utils.js build 019 attribution

--- ovice-utils.js
+++ ovice-utils.js
@@ -1,10 +1,11 @@
-// ovice utils build 018 by Tok@ovice, 2024 
+// ovice utils build 019 by Tok@ovice, 2024 
 var global_prm;
 var global_prm_val;
 var global_prf_country = 'en';
 var global_btn_position = '';
 var global_flg_ctype = {none:0,QP:1,LS:2,GL:3,XX:9};
 var global_flg_c = global_flg_ctype.none;
+var global_attribution = '';
 const className_UX_for_APAC = 'ux_for_apac';
 const className_UX_for_AU = 'ux_for_au';
 const className_UX_for_EN = 'ux_for_en';
@@ -117,6 +118,68 @@ function UXcustomizeViaCountry(){
   }
 }
 
+function attr(d) {
+  const refdata = `[
+    {"domain": "ovice.", "ref": "ovice"},
+    {"domain": "flexergylab.com", "ref": "ovice"},
+    {"domain": "play.google.com", "ref": "aso_google"},
+    {"domain": "apps.apple.com", "ref": "aso_apple"},
+    {"domain": "google.", "ref": "seo_google"},
+    {"domain": "yahoo.co.jp", "ref": "seo_yahoo"}, 
+    {"domain": "yahoo.com", "ref": "seo_yahoo"}, 
+    {"domain": "bing.com", "ref": "seo_bin"},
+    {"domain": "duckduckgo.com", "ref": "seo_duckduckgo"},
+    {"domain": "coccoc.com", "ref": "seo_coccoc"},
+    {"domain": "yandex.com", "ref": "seo_yandex"},
+    {"domain": "naver.com", "ref": "seo_naver"},
+    {"domain": "baidu.com", "ref": "seo_baidu"},
+    {"domain": "ecosia.org", "ref": "seo_ecosia"},
+    {"domain": "msn.com", "ref": "seo_msn"},
+    {"domain": "excite.co.jp", "ref": "seo_excite"},
+    {"domain": "goo.ne.jp", "ref": "seo_goo"},
+    {"domain": "livedoor.com", "ref": "seo_livedoor"},
+    {"domain": "biglobe.ne.jp", "ref": "seo_biglobe"},
+    {"domain": "ocn.ne.jp", "ref": "seo_ocn"},
+    {"domain": "nifty.com", "ref": "seo_nifty"},
+    {"domain": "infoseek.co.jp", "ref": "seo_infoseek"},
+    {"domain": "auone.jp", "ref": "seo_au"},
+    {"domain": "docomo.ne.jp", "ref": "seo_docomo"},
+    {"domain": "facebook.com", "ref": "social_facebook"},
+    {"domain": "x.com", "ref": "social_x"},
+    {"domain": "linkedin.com", "ref": "social_linkedin"},
+    {"domain": "youtube.com", "ref": "social_youtube"},
+    {"domain": "instagram.com", "ref": "social_instagram"},
+    {"domain": "www.itreview.jp", "ref": "review_itreview"},
+    {"domain": "boxil.jp", "ref": "review_boxil"},
+    {"domain": "it-trend.jp", "ref": "review_it-trend"},
+    {"domain": "capterra.jp", "ref": "review_capterra"},
+    {"domain": "capterra.com", "ref": "review_capterra"},
+    {"domain": "g2.com", "ref": "review_g2"},
+    {"domain": "getapp.com", "ref": "review_getapp"},
+    {"domain": "prtimes.jp", "ref": "media_prtimes"},
+    {"domain": "wantedly.com", "ref": "media_wantedly"},
+    {"domain": "techable.jp", "ref": "media_techable"},
+    {"domain": "zendesk.com", "ref": "media_zendesk"},
+    {"domain": "patentsalon.com", "ref": "media_patentsalon"},
+    {"domain": "note.com", "ref": "media_note"},
+    {"domain": "connpas.com", "ref": "media_connpas"},
+    {"domain": "voice-ping.com", "ref": "media_voice-ping"},
+    {"domain": "toremaga.com", "ref": "media_toremaga"},
+    {"domain": "wmr.tokyo", "ref": "media_wmr-tokyo"},
+    {"domain": "zdnet.com", "ref": "media_zdnet"},
+    {"domain": "cnet.com", "ref": "media_cnet"},
+    {"domain": "impress.co.jp", "ref": "media_impress"},
+    {"domain": "panora.tokyo", "ref": "media_panora-tokyo"},
+    {"domain": "notion.site", "ref": "media_notion"}
+  ]`;
+  var j = JSON.parse(refdata);
+  for (var i = 0; i < j.length; i++) {
+    if (d.indexOf(j[i].domain) > -1) {return j[i].ref;}
+  }
+  const u = new URL(d);
+  return 'other_' + u.host;
+}
+
 (function(){
   var str = retrieveGETqs();
   global_prm = str ? decodeURIComponent(str) : '';
@@ -167,7 +230,37 @@ function UXcustomizeViaCountry(){
     if(window.location.pathname.startsWith('/ja')) {global_prf_country = 'JP';}
     if(window.location.pathname.startsWith('/ko')) {global_prf_country = 'KR';}
   }
+
+  if ((typeof sessionStorage !== 'undefined') & (typeof localStorage !== 'undefined')) {
+    var ls = localStorage;
+    var ss = sessionStorage;
+    var r = document.referrer;
+    if (r === '') {r = 'direct';}
+    ls.setItem('ovicecom_cPages', Number(ls.getItem('ovicecom_cPages')) + 1);
+    if(ss.getItem('ovicecom_fEntry') === null) {
+      ss.setItem('ovicecom_fEntry', 1);
+      var v = Number(ls.getItem('ovicecom_cVisits'));
+      ls.setItem('ovicecom_cVisits', v + 1);
+      if (v === 0) {
+        ls.setItem('ovicecom_sFirstRef', r);
+      }
+      ls.setItem('ovicecom_sLastRef', r);
+      var t = new Date();
+      ls.setItem('ovicecom_nLastTime', t.getTime());
+    }
+    if (global_prm === '') {
+      ls.setItem('ovicecom_attribution', attr(r));
+      global_attribution = r;
+    } else {
+      var p = global_prm_val.get('source');
+      if (p !== null) {
+        ls.setItem('ovicecom_attribution', p);
+        global_attribution = p;
+      }
+    }
+  }
 })();
+
 $(function(){
     $(window).on('beforeunload', function() {
       if (global_flg_c == global_flg_ctype.GL || global_flg_c == global_flg_ctype.QP) {
@@ -182,9 +275,9 @@ $(function(){
     if (!target_url.startsWith('#') && !target_url.startsWith('?') && !target_url.includes('countrycode')) {
       if (global_flg_c == global_flg_ctype.GL || global_flg_c == global_flg_ctype.LS) {
         if (global_prm && !global_prm.includes('countrycode')) {
-          global_prm = global_prm + '&countrycode=' + global_prf_country;
+          global_prm = global_prm + '&countrycode=' + global_prf_country + (global_attribution !== '') ? '&attribution=' + global_attribution : '';
         } else {
-          global_prm = 'countrycode=' + global_prf_country;
+          global_prm = 'countrycode=' + global_prf_country + (global_attribution !== '') ? '&attribution=' + global_attribution : '';
         }
       }
       if (global_prm) {
